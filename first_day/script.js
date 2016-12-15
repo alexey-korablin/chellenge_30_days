5ccf2d5d7f0143b44fa77f407ac1d93304d4b2ad
Alexey Korablin <alexey_korablin@epam.com>
2016-12-15 12:02:02 +0300

    003 JavaScript Drum Kit. Optimize BG picture. Change link to BG pic in style.css. Add comments to the code in script.js

--- first_day/script.js
+++ first_day/script.js
@@ -10,21 +10,21 @@
             }
             audio.currentTime = 0;  // При каждом событии устанавливаем позиуию для воспроизведения файла на 0-ю секунду,
             audio.play(); // благодаря этому файл воспроизводится снова не дожидаясь окончания предыдущего воспроизведения
-            const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-            key.classList.add('key__pressed');
-        };
-
-        function removeTransition(e) {
-            if ( e.propertyName !== 'color') {
-                return;
-            }
-            this.classList.remove('key__pressed');
+            const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // сохраняем в константу key элемент на котором произошло событие
+            key.classList.add('key__pressed');  //добавляем к полученнму элементу класс key__pressed
         }
 
-        const keys = document.querySelectorAll('.key');
-        keys.forEach( key => key.addEventListener('transitionend', removeTransition) );
+        function removeColor(e) {  //Функция удаления класса key__pressed по событию
+            if ( e.propertyName !== 'color') {  //проверяем есть ли в полученном объекте события свойство со значением color. Если нет, то
+                return;  // прекращаем выполнение скрипта
+            }
+            this.classList.remove('key__pressed'); //Удаляем с текущего элемента класс key__pressed
+        }
 
-        document.addEventListener('keydown', playSound);
+        const keys = document.querySelectorAll('.key'); //получаем все элементы со страницы с классом key и сохраняем их в константу keys
+        keys.forEach( key => key.addEventListener('transitionend', removeColor) ); //для каждого элемента массива keys регистрируем event listener для события transitioned. Событие передаем функции removeColor
+
+        document.addEventListener('keydown', playSound);  //регистрируем event listener для события keydown на всем документе и передаем событие в функцию playSound
     };
-    window.onload = drumKit;
+    window.onload = drumKit; //Функция drumKit готова к работе после полной загрузки страницы.
 })();
